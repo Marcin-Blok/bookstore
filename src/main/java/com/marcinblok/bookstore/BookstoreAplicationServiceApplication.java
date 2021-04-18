59e0db7719a658c1ea22d9fdd3e8a81c2cbd66dd
Marcin-Blok <marcinblok7980@gmail.com>
2021-04-19 01:49:09 +0200

    Comit inicjujący repozytorium

---
+++ src/main/java/com/marcinblok/bookstore/BookstoreAplicationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.marcinblok.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreAplicationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreAplicationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcinblok/bookstore/BookstoreAplicationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcinblok.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreAplicationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
